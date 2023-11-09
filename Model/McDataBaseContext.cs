00f97b5cd13dd791897ec6843db222ed82dd5c50
Sala Digital (CtP/ETS) <disrct@bosch.com>
2023-11-09 13:58:18 -0300

    Add Model

---
+++ Model/McDataBaseContext.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace mcdonalds_api.Model;
+
+public partial class McDataBaseContext : DbContext
+{
+    public McDataBaseContext()
+    {
+    }
+
+    public McDataBaseContext(DbContextOptions<McDataBaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<ClientOrder> ClientOrders { get; set; }
+
+    public virtual DbSet<ClientOrderItem> ClientOrderItems { get; set; }
+
+    public virtual DbSet<MenuItem> MenuItems { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Store> Stores { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=CT-C-001YF\\SQLEXPRESS01;Initial Catalog=McDataBase;Integrated Security=True;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ClientOrder>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__ClientOr__3214EC27563B3C91");
+
+            entity.ToTable("ClientOrder");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.DeliveryMoment).HasColumnType("datetime");
+            entity.Property(e => e.Moment).HasColumnType("datetime");
+            entity.Property(e => e.OrderCode)
+                .IsRequired()
+                .HasMaxLength(12)
+                .IsUnicode(false);
+            entity.Property(e => e.StoreId).HasColumnName("StoreID");
+
+            entity.HasOne(d => d.Store).WithMany(p => p.ClientOrders)
+                .HasForeignKey(d => d.StoreId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__ClientOrd__Store__52593CB8");
+        });
+
+        modelBuilder.Entity<ClientOrderItem>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__ClientOr__3214EC27B6B606C6");
+
+            entity.ToTable("ClientOrderItem");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+            entity.HasOne(d => d.ClientOrder).WithMany(p => p.ClientOrderItems)
+                .HasForeignKey(d => d.ClientOrderId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__ClientOrd__Clien__5535A963");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.ClientOrderItems)
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__ClientOrd__Produ__5629CD9C");
+        });
+
+        modelBuilder.Entity<MenuItem>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__MenuItem__3214EC274D0027BA");
+
+            entity.ToTable("MenuItem");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Price).HasColumnType("decimal(5, 2)");
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.StoreId).HasColumnName("StoreID");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.MenuItems)
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__MenuItem__Produc__4E88ABD4");
+
+            entity.HasOne(d => d.Store).WithMany(p => p.MenuItems)
+                .HasForeignKey(d => d.StoreId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__MenuItem__StoreI__4F7CD00D");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Product__3214EC27C0EF6E8A");
+
+            entity.ToTable("Product");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.DescriptionText)
+                .IsRequired()
+                .HasMaxLength(400)
+                .IsUnicode(false);
+            entity.Property(e => e.ProductName)
+                .IsRequired()
+                .HasMaxLength(80)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Store>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Store__3214EC27A174EAA7");
+
+            entity.ToTable("Store");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Localization)
+                .IsRequired()
+                .HasMaxLength(200)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
